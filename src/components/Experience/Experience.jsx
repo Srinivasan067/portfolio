b8e77fdeb2d9254831503b5cedbb9c3a9d3ab6c5
srini <87656917+Srinivasan067@users.noreply.github.com>
2023-07-27 20:54:57 +0530

    Update Experience.jsx

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -9,12 +9,12 @@ const Experience = () => {
     <div className="experience" id='experience'>
       <div className="achievement">
         {/* darkMode */}
-        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>11+</div>
-        <span  style={{color: darkMode?'white':''}}>Months </span>
+        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>1+</div>
+        <span  style={{color: darkMode?'white':''}}>Year </span>
         <span>Experience</span>
       </div>
       <div className="achievement">
-        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>2+</div>
+        <div className="circle" style={{color: darkMode?'var(--orange)':''}}>8+</div>
         <span  style={{color: darkMode?'white':''}}>completed </span>
         <span>Projects</span>
       </div>
